fd3f533377aca0fdf4e207fb06b6d60bb2b84baf
HeiderGutierrez <hgutierrez@pratechgroup.com>
2021-09-20 07:09:18 -0500

    add - update - delete - search

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,69 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "@pnp/sp/search";
 import { sp } from '@pnp/sp/presets/all';
+import { IItemAddResult } from "@pnp/sp/items";
+import { ISearchQuery, SearchResults, SearchQueryBuilder } from "@pnp/sp/search";
+import './App.css';
 
 function App() {
   const [items, setItems] = useState<any[]>([]);
+  let campo = document.getElementById('field');
+
+  // AGREGAR
+  const addListItem = useCallback(async () => {
+    alert('me estan activando')
+    const addItem: IItemAddResult = await sp.web.lists.getByTitle("BaseDatosPersonas").items.add({
+      Title: campo
+    });
+    console.log('Item agregado:', addItem.data.Title);
+  }, []);
+
+  // MODIFICAR
+  const updateListItem = useCallback(async () => {
+    let list = sp.web.lists.getByTitle("BaseDatosPersonas");
+    const upadteItem = await list.items.getById(3).update({
+      Title: 'UpdateNewItem'
+    })
+    console.log('Item modificado:', upadteItem.data);
+  }, []);
+
+  // ELIMINAR
+  const deleteListItem = useCallback(async () => {
+    let list = sp.web.lists.getByTitle("BaseDatosPersonas");
+    const deleteItem = await list.items.getById(3).delete();
+    console.log('Item eliminado:', deleteItem);
+  }, []);
 
   const getListItems = useCallback(async () => {
-    try{
+    try {
       const items: any[] = await sp.web.lists.getByTitle('BaseDatosPersonas').items.select('Title, ID').getAll();
       setItems(items);
-    }catch (error){
-      console.log(error);    
+    } catch (error) {
+      console.log(error);
     }
-    }, []);
+  }, []);
+
+  // SEARCH
+  // AGREGAR
+  const searchItem = useCallback(async () => {
+    const result: SearchResults = await sp.search('BaseDatosPersonas');
+    console.log('resultados search', result.PrimarySearchResults);    
+  }, []);
 
   useEffect(() => {
+    // addListItem();
+    // updateListItem();
+    // deleteListItem();
+    searchItem();
     getListItems();
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <ul>
-          {items.map(x => <li style={{color: '#fff'}}>{x.Title}</li>)}
+        <ul className="list-view">
+          {items.map(x => <li style={{ color: '#fff' }} key={x.ID}>{x.Title}</li>)}
         </ul>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
